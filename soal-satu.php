4e863ec73a9efffbac2135523098b1cb4ff99e6c
Darapermatasarii <darapermatasari21@gmail.com>
2026-07-27 13:24:39 +0700

    TUGAS1

---
+++ soal-satu.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>String PHP</title>
+</head>
+<body>
+    <h1>Berlatih String PHP</h1>
+    <?php   
+        echo "<h3> Soal No 1</h3>";
+
+        $first_sentence = "Hello PHP!"; 
+        $second_sentence = "I'm ready for the challenges";
+        echo "Panjang String: " . strlen($first_sentence) . ", jumlah kata: " . str_word_count($first_sentence) . " Dari Kata $first_sentence <br>";
+
+        echo "Panjang String: " . strlen($second_sentence) . ", jumlah kata: " . str_word_count($second_sentence) . " Dari Kata $second_sentence <br>";
+        $first_sentence = "Hello PHP!" ; 
+        $second_sentence = "I'm ready for the challenges"; 
+
+        echo "<h3> Soal No 2</h3>";
+        
+        $string2 = "I love PHP";
+        
+        echo "<label>String: </label> \"$string2\" <br>";
+        echo "Kata pertama: " . substr($string2, 0, 1) . "<br>" ; 
+        echo "Kata kedua: " . substr($string2, 2, 4);
+        echo "<br> Kata Ketiga: " . substr($string2, 7, 3);
+
+        echo "<h3> Soal No 3 </h3>";
+
+        $string3 = "PHP is old but Good!";
+
+        echo "String: \"$string3\" <br>";
+        echo str_replace("Good!", "awesome", $string3);
+
+    ?>
+</body>
+</html>
